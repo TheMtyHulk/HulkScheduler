cae30b7f1da5e192cfb1bc50761f15c5306eb438
TheMtyHulk <TheMtyHulk@users.noreply.github.com>
2024-06-21 23:44:39 +0530

    chore : built scheduler

---
+++ scheduler.py
@@ -0,0 +1,40 @@
+import os
+import gridfs
+from pymongo import MongoClient
+from uuid import uuid4
+from datetime import datetime
+
+class scheduler:
+    def __init__(self):
+        client = MongoClient(os.getenv('MONGO_URL'))
+        self.db = client['taskmaster']
+        self.schedule("tasks")
+    
+    def schedule(self, directory):
+        db = self.db
+        tasks = db['tasks']
+        fs=gridfs.GridFS(db)
+        for name in os.listdir(directory):
+            uuid=str(uuid4())
+            if not name.endswith('.pdf'):
+                continue
+            with open(os.path.join(directory, name), 'rb') as f:
+                fs.put(f, filename=name,_id=uuid)
+            tasks.create_index("scheduled_at")
+            tasks.insert_one({
+				"_id": uuid,
+				"command": name,
+				"scheduled_at": datetime.now(),
+				"picked_at": None,
+				"started_at": None,
+				"completed_at": None,
+				"failed_at": None
+			})
+            
+        print("All PDF files have been uploaded to MongoDB and tasks have been scheduled.")
+
+
+#the below code is for testing purposes just uncomment it to test the scheduler alone
+
+# s=scheduler()
+# # s.schedule("tasks")
